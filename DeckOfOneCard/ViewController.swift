921643414b3f769440393f49033d7effa689c4e0
Uldis Zingis <uldis.zingis@makit.lv>
2016-10-10 09:52:26 -0600

    Initial Commit

---
+++ DeckOfOneCard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DeckOfOneCard
+//
+//  Created by Uldis Zingis on 10/10/16.
+//  Copyright © 2016 Uldis Zingis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
